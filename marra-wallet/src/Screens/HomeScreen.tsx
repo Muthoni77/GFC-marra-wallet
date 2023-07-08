2087fc7f8378fbaa6a3f7542b78d5f0d71bdb7cb
Muthoni77 <gloriamuthoni25@gmail.com>
2023-07-08 16:51:10 -0400

    Homescreen progress

--- marra-wallet/src/Screens/HomeScreen.tsx
+++ marra-wallet/src/Screens/HomeScreen.tsx
@@ -46,6 +46,152 @@ const HomeScreen = () => {
             {/* <Icons name="notifications" size={24} color={colors.text}/> */}
           </TouchableOpacity>
         </View>
+        <View style={{ flexDirection: "column", paddingHorizontal: 24, gap: 14 ,marginTop:20,}}>
+        <TouchableOpacity
+            style={{
+              flex: 1,
+              // width:100,
+              height:150,
+              borderRadius: 12,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              paddingHorizontal: 24,
+              flexDirection: "row",
+              gap: 12,
+             
+            }}
+          >
+           
+            <Text
+              style={{
+              
+                fontSize: 16,
+                color: colors.text,
+                opacity: 0.5,
+                alignItems: "center",
+                justifyContent:"center",
+                marginBottom:80,
+                paddingLeft:70
+                
+              }}
+              // numberOfLines={1}
+            >
+              Your Portfolio
+            </Text>
+            <View style={{ flexDirection: "row",  gap: 10,alignItems:"center" ,justifyContent:"center",position:"absolute",paddingHorizontal:80}}>
+           
+            <Icons
+              name="attach-money"
+              size={34}
+              color={colors.text}
+              style={{ opacity: 0.5 ,}}
+            />
+             <Text
+              style={{
+                // flex: 1,
+                fontSize: 34,
+                color: colors.text,
+                opacity: 0.5,
+              }}
+            >
+              450,937
+            </Text>
+            </View>
+            <Text
+              style={{
+             
+              fontSize: 16,
+                color: colors.text,
+                opacity: 0.5,
+                paddingHorizontal:24,
+              
+                marginTop:50,
+                textAlign:"center" ,
+                
+                
+              }}
+              
+            >
+            Monthly Profit
+            </Text>
+       
+          </TouchableOpacity>
+        </View>
+        <View>
+          {/* Search Bar Section */}
+          
+        <View style={{ flexDirection: "row", paddingHorizontal: 24, gap: 14 ,marginTop:20,}}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              width:100,
+              height:100,
+              borderRadius: 12,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              paddingHorizontal: 24,
+              flexDirection: "row",
+              gap: 12,
+            }}
+          >
+           
+            <Text
+              style={{
+                flex: 1,
+                fontSize: 16,
+                color: colors.text,
+                opacity: 0.5,
+              }}
+            >
+              Buy Crypto
+            </Text>
+            <Icons
+              name="credit-card"
+              size={24}
+              color={colors.text}
+              style={{ opacity: 0.5 }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              width:100,
+              height:100,
+              borderRadius: 12,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              paddingHorizontal: 24,
+              flexDirection: "row",
+              gap: 12,
+            }}
+          >
+           
+            <Text
+              style={{
+                flex: 1,
+                fontSize: 16,
+                color: colors.text,
+                opacity: 0.5,
+              }}
+            >
+              Sell Crypto
+            </Text>
+            <Icons
+              name="attach-money"
+              size={24}
+              color={colors.text}
+              style={{ opacity: 0.5 }}
+            />
+          </TouchableOpacity>
+
+        </View>
+
+        </View>
+        
       </SafeAreaView>
     </ScrollView>
   );
